53a9b4d99cb9b14d5ea25ed390a5864f12d19b7d
Jyothrilinga Rao <jyothri@users.noreply.github.com>
2020-11-09 16:41:01 -0800

    Add files via upload
    Added file to parse usage from ATT bill CSV dump.

---
+++ parse_csv_phones.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from collections import Counter
+import sys
+import csv
+import re
+
+def parse_file(file_path, output_file=sys.stdout):
+    print(f"Starting parse of file for SMS {file_path}")
+    ph_nos=[]
+    with open(file_path, newline='') as csvfile:
+      spamreader = csv.reader(csvfile, delimiter='|', quotechar=' ')
+      for row in spamreader:
+          if (len(row)>3):
+              x = re.search(r"\d{3}\.\d{3}\.\d{4}", row[3])
+              if x:
+                  ph_nos.append(x.group())
+      print("Number   |  Count ") 
+      for x,y in sorted(Counter(ph_nos).items(), key=lambda item: item[1], reverse=True):
+          print(x, y)
+
+def parse_phone_file(file_path, output_file=sys.stdout):
+    print(f"Starting parse of file with phone numbers {file_path}")
+    ans={}
+    with open(file_path, newline='') as csvfile:
+      spamreader = csv.reader(csvfile, delimiter='|', quotechar=' ')
+      for row in spamreader:
+          if (len(row)>3):
+              x = re.search(r"\d{3}\.\d{3}\.\d{4}", row[3])
+              if x:
+                  ph_no = x.group()
+                  minutes = int(row[6])
+                  ans[ph_no] = ans.get(ph_no,0) + minutes
+      print("Number | Minutes")
+      for x,y in sorted(ans.items(), key=lambda item: item[1], reverse=True):
+          print(x, y)
+
+if __name__ == "__main__":
+  if len(sys.argv)==2:
+      parse_file(sys.argv[1])
+  else:
+      parse_phone_file(sys.argv[1]);
